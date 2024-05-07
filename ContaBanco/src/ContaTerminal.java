a640d6589239aa4aa74a38d63707697ff12f07a2
ThiagoPeres6 <thiago.s.peres@hotmail.com>
2024-05-07 20:00:19 -0300

    Desenvolvido o código da simulação de conta bancária através do terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
+        //TODO: conhecer e importar a classe Scanner
 
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //TODO: Exibir as mensagens para o nosso usuário
+        //TODO: Obter pelo scanner os valores digitados no terminal
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o primeiro nome do Cliente:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o Saldo:");
+        double saldoConta = scanner.nextDouble();
+
+        //TODO: exibir a mensagem conta criada
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", sua conta é " + numeroConta + " e seu saldo de R$" + saldoConta + " já está disponível para saque.");
 
     }
 }
