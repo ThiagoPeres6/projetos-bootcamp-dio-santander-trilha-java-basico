5e153b2569e190919b6531f8bedbdc09301bf1f4
ThiagoPeres6 <thiago.s.peres@hotmail.com>
2024-05-08 11:27:16 -0300

    Criação dos métodos da ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+package ContaBanco.src;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,21 +13,45 @@ public class ContaTerminal {
         //TODO: Exibir as mensagens para o nosso usuário
         //TODO: Obter pelo scanner os valores digitados no terminal
 
-        System.out.println("Por favor, digite o número da Conta:");
-        int numeroConta = scanner.nextInt();
+        int numeroConta = getNumeroConta(scanner);
 
-        System.out.println("Por favor, digite o número da Agência:");
-        String agencia = scanner.next();
+        String agencia = getString(scanner);
 
-        System.out.println("Por favor, digite o primeiro nome do Cliente:");
-        String nomeCliente = scanner.next();
+        String nomeCliente = getNomeCliente(scanner);
 
-        System.out.println("Por favor, digite o Saldo:");
-        double saldoConta = scanner.nextDouble();
+        double saldoConta = getSaldoConta(scanner);
 
         //TODO: exibir a mensagem conta criada
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", sua conta é " + numeroConta + " e seu saldo de R$" + saldoConta + " já está disponível para saque.");
+        mensagemFinal(nomeCliente, agencia, numeroConta, saldoConta);
 
+    }
+
+    private static void mensagemFinal(String nomeCliente, String agencia, int numeroConta, double saldoConta) {
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", sua conta é: " + numeroConta + " e seu saldo de R$" + saldoConta + " já está disponível para saque.");
+    }
+
+    private static double getSaldoConta(Scanner scanner) {
+        System.out.println("Por favor, digite o Saldo:");
+        double saldoConta = scanner.nextDouble();
+        return saldoConta;
+    }
+
+    private static String getNomeCliente(Scanner scanner) {
+        System.out.println("Por favor, digite o primeiro nome do Cliente:");
+        String nomeCliente = scanner.next();
+        return nomeCliente;
+    }
+
+    private static String getString(Scanner scanner) {
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+        return agencia;
+    }
+
+    private static int getNumeroConta(Scanner scanner) {
+        System.out.println("Por favor, digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+        return numeroConta;
     }
 }
